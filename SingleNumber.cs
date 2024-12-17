cd976f9d2c8674198a79858fa9368617433e4321
Hovhannes Hakobyan <hovhannes.hakobyan.a@gmail.com>
2024-12-17 12:19:35 +0400

    Add files via upload

---
+++ SingleNumber.cs
@@ -0,0 +1,20 @@
+﻿public class Solution
+{
+    public int SingleNumber(int[] nums)
+    {
+        HashSet<int> set = new HashSet<int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (!set.Contains(nums[i]))
+            {
+                set.Add(nums[i]);
+            }
+            else
+            {
+                set.Remove(nums[i]);
+            }
+        }
+        return set.First();
+    }
+}
